6b4c0cf3520186202f76d49629f375d6b265fc1c
Misael de Castro Bezerra <157545220+Misaeldecastro@users.noreply.github.com>
2024-02-17 23:51:02 -0300

    Add files via upload

---
+++ Ar condicionado.py
@@ -0,0 +1,8 @@
+#codigo com estrutura simples IF
+temperatura = (int(input("Temperatura: ")))
+if temperatura>25:
+    print("Ambiente quente, ajustando ar-condicionado")
+else:
+    print("ambiente ok, não precisa ser ajustado")
+
+print("Ligando o ar-condicionado")
